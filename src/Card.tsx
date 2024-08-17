3793efd38d6dc0b639674fcaec35394b2308d04d
MgenGlder <mgen.glder@gmail.com>
2024-08-17 11:35:50 -0400

    Fix mentioned build errors

--- src/Card.tsx
+++ src/Card.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import { ReactNode } from "react";
 
 export default function Card({ children }: { children: ReactNode }) {
     return (
       <div className="support__point">
-        <img className="sp__image" src={null} />
+        <img className="sp__image" />
         <div className="sp__text">
           {children}
         </div>
