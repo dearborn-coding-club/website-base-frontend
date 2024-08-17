d0ffdb4736f09af7491956860053b0c59a7b9af1
Daniel X <xuboxdan@gmail.com>
2024-08-17 12:59:16 -0400

    Added wrong foo tags

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,6 +4,7 @@ export default function Card({ children }: { children: ReactNode }) {
   return (
     <div className="support__point">
       <img className="sp__image" />
+      <foo></foo>
       <div className="sp__text">{children}</div>
     </div>
   );
