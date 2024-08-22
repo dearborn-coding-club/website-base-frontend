df63760da9cc73c7a3dbdb3ad5f4821d3407520f
Daniel X <xuboxdan@gmail.com>
2024-08-22 15:59:06 -0400

    Removed foo tags that were present for testing

--- src/Card.tsx
+++ src/Card.tsx
@@ -4,7 +4,6 @@ export default function Card({ children }: { children: ReactNode }) {
   return (
     <div className="support__point">
       <img className="sp__image" />
-      <foo></foo>
       <div className="sp__text">{children}</div>
     </div>
   );
