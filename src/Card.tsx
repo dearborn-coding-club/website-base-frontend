328d17efa7d749d5352de43f69f1998582ae44d6
Daniel X <xuboxdan@gmail.com>
2024-08-31 12:41:25 -0400

    Blah

--- src/Card.tsx
+++ src/Card.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 export default function Card({ children }: { children: ReactNode }) {
   return (
     <div className="support__point">
+      <Foo />
       <img className="sp__image" />
       <div className="sp__text">{children}</div>
     </div>
