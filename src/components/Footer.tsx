11be74b06da1e359b8110732ed50a33b005941e7
MgenGlder <mgen.glder@gmail.com>
2025-02-10 15:21:06 -0500

    feat: Clean up footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,10 +4,12 @@ import ig from "../assets/ig.png"
 export default function Footer() {
   return (
     <div className="Footer">
-      <div className="footer-col">
-        <small>
-          &#127968; Meetup address: <a href="https://maps.app.goo.gl/ZvxnMHjYoDRCD7V27">13810 Michigan Ave, Dearborn, MI 48126</a>
-        </small>
+      <div className="custom-footer-col">
+        <div className="section-1">Reach out to us</div>
+          <div className="section-2"><small>&#127968; Meetup address: </small></div>
+          <div className="section-3"><a href="https://maps.app.goo.gl/ZvxnMHjYoDRCD7V27">13810 Michigan Ave, Dearborn, MI 48126</a></div>
+        <div className="section-4"><small>Meetup event: </small></div>
+        <div className="section-5"><a href="https://www.meetup.com/dearborn-coding-club/">https://www.meetup.com/dearborn-coding-club/</a></div>
       </div>
 
       <div className="footer-col">
